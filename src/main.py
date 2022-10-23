7b3ffa7172f67be32dac952e28f264ae0164259c
Anmol Kumar <31875086+AzvenusK@users.noreply.github.com>
2022-10-23 04:35:42 +0100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from distutils.log import debug
+import speech_recognition as sr
+
+print('Test')
+listner = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print('Listening...')
+        voice = listner.listen(source)
+        com = listner.recognize_google(voice)
+        print(com)
+except Exception as expt:
+    print(expt)
