1f31df7e6bb01ca709fecc0137d6ba0fa14763f5
Anmol Kumar <31875086+AzvenusK@users.noreply.github.com>
2022-12-22 18:07:14 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,36 @@
 from distutils.log import debug
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
 
 listner = sr.Recognizer()
-try:
-    with sr.Microphone() as source:
-        print('Listening...')
-        voice = listner.listen(source)
-        com = listner.recognize_google(voice)
-        print(com)
-except Exception as expt:
-    print(expt)
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[10].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def take_commmand():       
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listner.listen(source)
+            com = listner.recognize_google(voice)   
+            com = com.lower()
+            #if 'annie' in com:
+            #    print(com)
+    except Exception as expt:
+        print(expt)
+    return com
+
+def run_annie():
+    command = take_commmand()
+    print(command)
+    if 'play' in command:
+        command = command.replace('hey','')
+        command = command.replace('arnold', '')
+        command = command.replace('play','')
+        talk('Playing Now'+command)
+        
+
+run_annie()
