e7597b3e9b5db704ce55c64844a9ada17dec2508
Anmol Kumar <31875086+AzvenusK@users.noreply.github.com>
2022-12-23 01:24:20 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
-from distutils.log import debug
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
+import wikipedia
+from distutils.log import debug
+from googlesearch import search
 
 listner = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', 'english_rp+f4')
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
 def take_commmand():       
     try:
         with sr.Microphone() as source:
@@ -29,14 +33,34 @@ def run_annie():
         talk('Yeah how can I help?')
         command = take_commmand()
         if 'play' in command:
-            command = command.replace('hey','')
-            command = command.replace('annie', '')
             command = command.replace('play','')
             talk('Playing Now'+command)
             pywhatkit.playonyt(command)
         elif 'time' in command:
             time = datetime.datetime.now().strftime('%I:%M %p')
             talk('Right now it is'+time)
+        elif 'wiki' or 'wikipedia' or 'info' or 'information' or 'tell me about' or 'who is' in command:
+            if 'of' in command:
+                command[command.find('of'):]
+                command = command.replace('of','')
+            elif 'on' in command:
+                command[command.find('on'):]
+                command = command.replace('on','')
+            elif 'about' in command:
+                command[command.find('about'):]
+                command = command.replace('about','')
+            elif 'who is' in command:
+                command[command.find('is'):]
+                command = command.replace('is','')
+            print(command)
+            info = wikipedia.summary(command, 2)
+            print(info)
+            talk(info)
+            
+        else:
+            talk('Here is what I found on the internet')
+            for searchresults in search(command, tld="co.uk", num = 10, stop = 10, pause = 2):
+                print(searchresults)
     else:
         exit
 
